df6ad11a3e3637d25da45f36b58f193f739998bf
Louie Lu <git@louie.lu>
2018-10-09 21:12:39 +0800

    Update run script to multiple MCS

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for mcs in 0 1 2 4 8
+for mcs in 0 1 2 4 7 8 9 10 12 15
 do
 	sudo sudo iw dev wlp1s0 set bitrates ht-mcs-5 $mcs
 	for round in 0 1 2
 	do
-		echo "802.11n / 20 MHz / MCS $mcs"
-		python measure.py "802.11n / 20 MHz / MCS $mcs"
+		echo "802.11n / 20 MHz / MCS $mcs / 3TP / Bulk4M"
+		python measure.py --title "802.11n / 20 MHz / MCS $mcs / 3TP / Bulk4M" 192.168.5.41 192.168.5.42 192.168.5.43
 	done
 done
