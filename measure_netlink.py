2a91944a91c628d99d94df0599c270d6853809ba
Louie Lu <git@louie.lu>
2018-10-22 18:55:45 +0800

    Use epoll to handle UDP

--- measure_netlink.py
+++ measure_netlink.py
@@ -7,6 +7,7 @@ import subprocess
 import statistics
 import math
 import os
+import select
 import matplotlib
 from collections import defaultdict
 from concurrent.futures import ProcessPoolExecutor
@@ -18,23 +19,31 @@ from matplotlib import pyplot as plt
 NETLINK_NEMS_ATH9K = 31
 NETLINK_NEMS_ATH9K_GROUP = 32
 NETLINK_BUF_LENGTH = 128
-TIMEOUT = 5
+TIMEOUT = 2
 sock = None
+epoll = None
 
 # IRTT Settings
-SECONDS = '30s'
+SECONDS = '15s'
 arp_regex = r'(\b(?:\d{1,3}\.){3}\d{1,3}\b)|(\b([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})\b)'
 
 # Matplotlib Settings
 font = FontProperties()
 font.set_family('monospace')
 font.set_size('small')
+COLOR_PATTLE = {
+    '192.168.5.41': 'blue',
+    '192.168.5.42': 'green',
+    '192.168.5.43': 'red'
+}
 
 # Which diff
 DIFF_BY_ENQ = True
 
 
 def cleanup_sock():
+    epoll.unregister(sock.fileno())
+    epoll.close()
     sock.close()
 
 
@@ -57,12 +66,21 @@ def calculate_diffs(output: list):
     enq_timestamps = defaultdict(dict)
     tx_timestamps = defaultdict(dict)
     ack_timestamps = defaultdict(dict)
+    wake_timestamps = defaultdict(dict)
+    aggr_timestamps = defaultdict(dict)
     diff_timestamps = defaultdict(lambda: defaultdict(list))
 
     for i in output:
         addr, seqno, timestamp, mode = i.split('|')
         if mode == 'enq':
             enq_timestamps[addr][seqno] = int(timestamp)
+        elif mode == 'wake':
+            wake_timestamps[addr][seqno] = int(timestamp)
+        elif mode == 'aggr':
+            aggr_timestamps[addr][seqno] = int(timestamp)
+            if seqno in wake_timestamps[addr]:
+                diff_timestamps['aggr'][addr].append(
+                    (aggr_timestamps[addr][seqno] - wake_timestamps[addr][seqno]) / 1000)
         elif mode == 'tx':
             tx_timestamps[addr][seqno] = int(timestamp)
         elif mode == 'ack':
@@ -92,6 +110,7 @@ def get_result_text(ip, diff):
     diff.sort()
     texts = [
         f'host: {ip}',
+        f'packets:{len(diff)}',
         f'min:    {min(diff):.3f}',
         f'mean:   {statistics.mean(diff):.3f}',
         f'median: {statistics.median(diff):.3f}',
@@ -109,45 +128,61 @@ def do_figure(output_path: str, diffs: dict):
     ax2 = figure.add_subplot(212)
     ax2.axis('off')
     ax1.set_title(f'TX/ACK of 100 bytes UDP packets within 30 seconds\n{output_path}')
-    ax1.set_xscale('log')
-    ax1.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-    ax1.set_xlim((0.1, 500))
-    ax1.set_xticks([0.1, 1, 10, 100, 500])
+    #ax1.set_xscale('log')
+    #ax1.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    ax1.set_xlim((0, 50))
+    #ax1.set_xticks([0, 10, 25, 50])
+
+    ip_texts = {}
     for index, host in enumerate(diffs):
         time_diff = diffs[host]
         ip = arp_table[host]
         print(f'host: {ip}, packets: {len(time_diff)}')
-        ax1.hist(time_diff, len(set(time_diff)), density=True,
-                 cumulative=True, label=ip, histtype='step')
+        ax1.hist(time_diff, bins='auto', density=True,
+                 cumulative=True, label=ip, histtype='step',
+                 color=COLOR_PATTLE[ip])
         props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
         texts = '\n'.join(get_result_text(ip, time_diff))
-        ax2.text(index * 0.3, 0.5, texts, fontsize=10, bbox=props,
+        ip_texts[ip] = texts
+
+    for index, ip in enumerate(sorted(list(ip_texts.keys()))):
+        ax2.text(index * 0.3, 0.5, ip_texts[ip], fontsize=10, bbox=props,
                  verticalalignment='center', fontproperties=font)
+
     legend = ax1.legend(loc='center left', bbox_to_anchor=(1.04, 0.8))
-    plt.savefig(f'{output_path}.png', bbox_extra_artists=(legend,), bbox_inches='tight')
+    plt.savefig(f'{output_path}.png',
+                bbox_extra_artists=(legend,), bbox_inches='tight')
 
 
 def do_netlink(subtitle):
-    global sock
+    global sock, epoll
     sock = socket.socket(socket.AF_NETLINK,
                          socket.SOCK_RAW, NETLINK_NEMS_ATH9K)
     sock.bind((0, 0))
     sock.setsockopt(270, 1, NETLINK_NEMS_ATH9K_GROUP)
+    sock.setblocking(0)
     atexit.register(cleanup_sock)
 
+    epoll = select.epoll()
+    epoll.register(sock.fileno(), select.EPOLLIN)
     output = []
     try:
         while True:
-            b = sock.recvfrom(NETLINK_BUF_LENGTH)[0]
-            b = b[b.find(b'[') + 1: b.find(b']')].decode('utf-8')
-            sock.settimeout(TIMEOUT)
-            output.append(b)
+            events = epoll.poll(timeout=TIMEOUT)
+            if not events:
+                break
+            for fileno, event in events:
+                if event & select.EPOLLIN:
+                    b = sock.recvfrom(NETLINK_BUF_LENGTH)[0]
+                    b = b[b.find(b'[') + 1: b.find(b']')].decode('utf-8')
+                    output.append(b)
     except socket.timeout:
         sock.close()
 
     diffs = calculate_diffs(output)
     do_figure('enq_' + subtitle, diffs['enq'])
     do_figure('tx_' + subtitle, diffs['tx'])
+    do_figure('aggr_' + subtitle, diffs['aggr'])
 
 
 def run(args):
--- run_netlink.sh
+++ run_netlink.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-IPERF_BITRATE="60M"
-IPERF_PARALLEL="5"
-IPERF_PROTOCOL="tcp"
+IPERF_BITRATE="25M"
+IPERF_PARALLEL="4"
+IPERF_PROTOCOL="udp"
+RUNNING_TIME="17"
 
 if [ "$IPERF_PROTOCOL" == "tcp" ]; then
 	IPERF_OPTIONS=""
-elif [ "$IPERF_PROTOCOL" == "udp"]; then
+elif [ "$IPERF_PROTOCOL" == "udp" ]; then
 	IPERF_OPTIONS="-u"
 fi
 
 
-TITLE_1="802.11n / 40 MHz / MCS "
+TITLE_1="802.11n / 20 MHz / MCS "
 TITLE_2=" / 3TP / Bulk $IPERF_BITRATE x $IPERF_PARALLEL - $IPERF_PROTOCOL"
 
 
-for mcs in 0 1 2 4 7 8 9 10 12 15
+for mcs in 0
 do
 	sudo sudo iw dev wlp1s0 set bitrates ht-mcs-5 $mcs
-	for round in 0 1 2
+	for round in 0
 	do
 		TITLE="$TITLE_1$mcs$TITLE_2"
 		echo $TITLE
-		iperf3 -c 192.168.5.16 -i 1 -t 0 -b $IPERF_BITRATE -P $IPERF_PARALLEL -S 0x0 $IPERF_OPTIONS > /dev/null &
-		sudo ./measure_netlink.py --title "$TITLE" 192.168.5.41 192.168.5.42 192.168.5.43
+		iperf3 -c 192.168.5.16 -i 1 -t $RUNNING_TIME -b $IPERF_BITRATE -P $IPERF_PARALLEL -S 0x0 $IPERF_OPTIONS > /dev/null &
+		iperf3 -c 192.168.5.42 -i 1 -t $RUNNING_TIME -b $IPERF_BITRATE -P $IPERF_PARALLEL -S 0x0 $IPERF_OPTIONS > /dev/null &
+		iperf3 -c 192.168.5.43 -i 1 -t $RUNNING_TIME -b $IPERF_BITRATE -P $IPERF_PARALLEL -S 0x0 $IPERF_OPTIONS > /dev/null &
+		sudo ./measure_netlink.py --title "$TITLE" 192.168.5.41
 		pkill iperf3
+		sleep 3
 	done
 done
