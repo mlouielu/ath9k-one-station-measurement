9df40fc43f0f00bb37e486500e4b91f09903319d
Louie Lu <git@louie.lu>
2018-11-29 14:06:10 +0800

    Add new ip

--- run_bulk_flow.sh
+++ run_bulk_flow.sh
@@ -4,6 +4,7 @@ RATE=500M
 STA1=192.168.5.41
 STA2=192.168.5.42
 STA3=192.168.5.43
+STA4=192.168.5.44
 
 if [ "$1" -eq "0" ];
 then
@@ -24,3 +25,9 @@ if [ "$1" -ge "3" ];
 then
 	iperf3 -c $STA3 -i 1 -t 0 --fq-rate $RATE -J --logfile $2_iperf3 &
 fi
+
+if [ "$1" -ge "4" ];
+then
+	iperf3 -c $STA4 -i 1 -t 0 --fq-rate $RATE -J --locfile $2_iperf4 &
+fi
+
